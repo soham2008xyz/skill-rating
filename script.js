02c1e134e5203fb0050e44b8448097bba203abe0
Soham Banerjee <soham2008xyz@gmail.com>
2014-08-13 12:43:08 +0530

    Update navigation

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
 $(function(){
     $('a[title]').tooltip();
+    $('.button-wrapper').tooltip();
+});
+
+$('#user-type-next').click(function(e) {
+    e.stopPropagation();
+    $('#skill-type-button').trigger('click');
+});
+
+$('#skill-type-next').click(function(e) {
+    e.stopPropagation();
+    $('#rating-button').trigger('click');
+});
+
+$('#rating-next').click(function(e) {
+    e.stopPropagation();
+    $('#confirm-button').trigger('click');
+});
+
+$('#skill-type-prev').click(function(e) {
+    e.stopPropagation();
+    $('#user-type-button').trigger('click');
+});
+
+$('#rating-prev').click(function(e) {
+    e.stopPropagation();
+    $('#skill-type-button').trigger('click');
+});
+
+$('#confirm-prev').click(function(e) {
+    e.stopPropagation();
+    $('#rating-button').trigger('click');
 });

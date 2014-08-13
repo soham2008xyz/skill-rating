cdca43a3d9492a79292a0c9d4430887bc3c62953
Soham Banerjee <soham2008xyz@gmail.com>
2014-08-13 13:53:50 +0530

    Add basic selectpicker

--- script.js
+++ script.js
@@ -1,34 +1,41 @@
-$(function(){
+/* Tooltips on tab headings */
+
+$(function () {
     $('a[title]').tooltip();
-    $('.button-wrapper').tooltip();
 });
 
-$('#user-type-next').click(function(e) {
+/* Tab pane navigation */
+
+$('#user-type-next').click(function (e) {
     e.stopPropagation();
     $('#skill-type-button').trigger('click');
 });
 
-$('#skill-type-next').click(function(e) {
+$('#skill-type-next').click(function (e) {
     e.stopPropagation();
     $('#rating-button').trigger('click');
 });
 
-$('#rating-next').click(function(e) {
+$('#rating-next').click(function (e) {
     e.stopPropagation();
     $('#confirm-button').trigger('click');
 });
 
-$('#skill-type-prev').click(function(e) {
+$('#skill-type-prev').click(function (e) {
     e.stopPropagation();
     $('#user-type-button').trigger('click');
 });
 
-$('#rating-prev').click(function(e) {
+$('#rating-prev').click(function (e) {
     e.stopPropagation();
     $('#skill-type-button').trigger('click');
 });
 
-$('#confirm-prev').click(function(e) {
+$('#confirm-prev').click(function (e) {
     e.stopPropagation();
     $('#rating-button').trigger('click');
 });
+
+/* Bootstrap select */
+
+$('.selectpicker').selectpicker();
